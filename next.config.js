dab9353d1938b114403116ddf2a53fb1661f6fdd
Ovi Savescu <ovi@ovswebsites.com>
2022-03-10 09:58:52 +0200

    got icons working again

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: false,
+            replaceAttrValues: {
+              "#000": "currentColor",
+            },
+            svgProps: { focusable: "false" },
+            titleProp: true,
+          },
+        },
+      ],
+    })
+
+    return config
+  },
+}
