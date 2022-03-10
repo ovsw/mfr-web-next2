b95c4d3851db5184e5cab7d34f447e9b016a7c53
Ovi Savescu <ovi@ovswebsites.com>
2022-03-10 09:12:59 +0200

    refined blockiterator empty states

--- utils/blockIterator.js
+++ utils/blockIterator.js
@@ -7,10 +7,24 @@ import { StoryblokComponent, storyblokEditable } from "@storyblok/react"
 
 export function blockIterator(bloksArr) {
   // check that array is not empty
-  if (bloksArr.length == 0) {
-    return <p>NO BLOCKS TO RENDER</p>
-  }
 
+  if (bloksArr == undefined) {
+    return (
+      <div className="flex items-center justify-center py-10">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold py-10">Page has no content 😅</h1>
+          <p className="text-3xl mb-10">please add some?...</p>
+        </div>
+      </div>
+    )
+  }
+  if (bloksArr.length == 0) {
+    return (
+      <p className="mx-auto max-w-2xl text-bold py-10">
+        No content here ༼ つ ◕_◕ ༽つ ... please add some{" "}
+      </p>
+    )
+  }
   const output = bloksArr.map(blok => (
     <StoryblokComponent
       blok={blok}
