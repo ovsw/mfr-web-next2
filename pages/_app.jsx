4dbe158c9a6231042bb236bc36b24278c759a954
Ovi Savescu <ovi@ovswebsites.com>
2022-03-10 10:07:20 +0200

    added hash scrolling effect

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,8 @@
 import { storyblokInit, apiPlugin } from "@storyblok/react"
-
+import { useEffect } from "react"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+
 
 // import Teaser from "../components/teaser"
 // import Feature from "../components/feature"
@@ -85,6 +86,21 @@ storyblokInit({
 })
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const hash = window.location.hash
+    if (hash) {
+      setTimeout(() => {
+        document.querySelector(hash).scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+          inline: "nearest",
+        })
+      }, 100)
+    }
+  })
+
   return (
     <>
       <Header />
