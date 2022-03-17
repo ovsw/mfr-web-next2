1216604adfb56445a498dc8409447678800b343e
Ovi Savescu <ovi@ovswebsites.com>
2022-03-17 13:40:38 +0200

    three steps tablet styles done

--- components/bloks/row-three-steps.js
+++ components/bloks/row-three-steps.js
@@ -21,33 +21,38 @@ const RowThreeSteps = ({ blok: row }) => {
   return (
     <div className={textStyles}>
       {/* <h2>This is a thre column row</h2> */}
-      <div className="STEPS-ROW-WRAPPER sm:flex sm:flex-col md:flex-row sm:space-y-14 md:space-y-0 sm:mb-28 relative z-30 ">
+      <div className="STEPS-ROW-WRAPPER sm:flex sm:flex-col md:flex-row sm:space-y-14 md:space-y-0 md:space-x-5 lg:space-x-20 sm:mb-28 relative z-30 ">
+        {/* ITEM */}
         {row.steps.map((step, i) => {
           return (
             <div className="w-full md:w-1/3" key={i}>
-              <div className="hidden w-[80px] md:block md:w-[160px] mx-auto pb-16">
+              <div className="hidden w-[80px] md:block md:w-[160px] mx-auto pb-8">
                 <Image src={step.image?.filename} width="200" />
               </div>
 
-              <div className="md:pl-20">
-                <h3 className="text-3xl flex align-baseline">
+              <div className="">
+                <h3 className="text-3xl flex items-start">
                   <span
-                    className="bg-accent rounded-full w-[1.7em] leading-[1.4em] text-center text-white mr-4"
+                    className="bg-accent text-white rounded-full 
+                    w-10 h-10 
+                    text-center text-xl  mr-4
+                    flex justify-center items-center"
                   >
                     {i + 1}
                   </span>
 
-                  <span className="inline-block leading-[1.4em] py-1">
+                  <span className="inline-block py-1 text-2xl lg:font-semibold">
                     {step.title}
                   </span>
                 </h3>
-                <div className="pl-20 md:pl-14 pt-4 custom-list-bullets text-lg">
+                <div className="pl-8 pt-2 custom-list-bullets !text-base md:text-lg">
                   <RichTextRenderer data={step.description} className="" />
                 </div>
               </div>
             </div>
           )
         })}
+        {/* END ITEM */}
       </div>
 
       {/* dotted path */}
