8c2cd61c6262e657eb1944c303a0246826028d9d
OVIS <ovi@ovswebsites.com>
2022-03-31 12:54:29 +0300

    added axe accessibility tool

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import * as React from "react"
 import { storyblokInit, apiPlugin } from "@storyblok/react"
 import { useEffect } from "react"
 import Header from "../components/Header"
@@ -89,6 +90,26 @@ storyblokInit({
     "hero-product": HeroProduct,
   },
 })
+
+// Determines if we are running on server or in client.
+const isServerSideRendered = () => {
+  return typeof window === "undefined"
+}
+/**
+ * Accessibility tool - outputs to devtools console on dev only and client-side only.
+ * @see https://github.com/dequelabs/axe-core-npm
+ */
+if (process.env.NODE_ENV !== "production" && !isServerSideRendered()) {
+  // we import react-dom and @axe-core/react dynamically
+  // so that we'll receive warning in our console about
+  // inaccessible code.
+
+  import("react-dom").then(ReactDOM => {
+    import("@axe-core/react").then(axe => {
+      axe.default(React, ReactDOM, 1000, {})
+    })
+  })
+}
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
