9f8c8fe2bb4b870fce49b154ea6ef97b014a30c1
OVIS <ovi@ovswebsites.com>
2022-04-01 14:07:53 +0300

    removed console.log

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,8 +17,6 @@ export default function Home({ story: initialStory, recentPosts }) {
   //   return <div>Loading...</div>
   // }
 
-  console.log("seo", story.content)
-
   return (
     <>
       {/* <Head>
