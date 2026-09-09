9423013028f37c85a2a91c2184cebddac40935e4
Ovi Savescu <ovi@ovswebsites.com>
2026-09-09 12:24:55 +0300

    Publish static Marianna dispute provisions for Terms reference

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/legal/dispute-provisions",
+        destination: "/legal/dispute-provisions/index.html",
+      },
+    ]
+  },
   async redirects() {
     return [
       {
